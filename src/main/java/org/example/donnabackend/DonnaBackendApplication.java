d9b804cb9d09efb458eb07b666ee29e0c3fce5e5
Wissal <wissalchakir7@gmail.com>
2024-08-13 20:32:56 +0100

    debut du projet

---
+++ src/main/java/org/example/donnabackend/DonnaBackendApplication.java
@@ -0,0 +1,14 @@
+package org.example.donnabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonnaBackendApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonnaBackendApplication.class, args);
+	}
+
+}
